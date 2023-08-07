1a4aaf4e174893998e863e6d489b9d216f29e0e9
Nikita Averyanov <rac1onal@yandex.ru>
2023-08-07 22:20:12 +0300

    feat: создать модель movie

---
+++ models/movie.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+  moviedId: {
+    type: Number,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    // ref: 'user',
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL постера',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL трейлера',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL миниатюры постера',
+    },
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('movie', movieSchema);
