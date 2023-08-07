93450e41bc79cdb3b5045ae92ffcca7cc0ecac87
Nikita Averyanov <rac1onal@yandex.ru>
2023-08-07 22:02:55 +0300

    feat: написать схему user, установить пакеты bcrypt, validator, создать класс ошибки 401

---
+++ models/user.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+// Подключим модуль для хэширования пароля
+// eslint-disable-next-line import/no-unresolved
+const bcrypt = require('bcryptjs');
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const isEmail = require('validator/lib/isEmail');
+
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: [2, 'Минимальная длина поля "name" - 2'],
+    maxlength: [30, 'Максимальная длина поля "name" - 30'],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Некорректный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+}, {
+  // добавим, чтобы при запросе не приходил пароль пользователя
+  toJSON: { useProjection: true },
+  toObject: { useProjection: true },
+}, { versionKey: false });
+
+// Кастомный метод проверки email и пароля
+// eslint-disable-next-line func-names
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(
+        password,
+        user.password,
+      )
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
