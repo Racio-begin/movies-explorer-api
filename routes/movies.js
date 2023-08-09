ae3c7fab2b3ddd21505af652e546f6b4ea714dc3
Nikita Averyanov <rac1onal@yandex.ru>
2023-08-09 20:07:02 +0300

    feat: создать роуты фильмов

--- routes/movies.js
+++ routes/movies.js
@@ -1,3 +1,20 @@
 const moviesRouter = require('express').Router();
 
+const {
+  getMovies,
+  createMovies,
+  deleteMovies,
+} = require('../controllers/movies');
+
+// возвращает все сохранённые текущим пользователем фильмы
+moviesRouter.get('/movies', getMovies);
+
+// создаёт фильм с переданными в теле
+// country, director, duration, year, description,
+// image, trailer, nameRU, nameEN и thumbnail, movieId
+moviesRouter.post('/movies', createMovies);
+
+// удаляет сохранённый фильм по id
+moviesRouter.delete('/movies/_id', deleteMovies);
+
 module.exports = moviesRouter;
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,10 @@ const {
   updateUser,
 } = require('../controllers/users');
 
+// возвращает информацию о пользователе (email и имя)
 usersRouter.get('/me', getUserInfo);
+
+// обновляет информацию о пользователе (email и имя)
 usersRouter.patch('/me', updateUserInfoJoiValidation, updateUser);
 
 module.exports = usersRouter;
