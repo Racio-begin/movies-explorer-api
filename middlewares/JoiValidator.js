96855a8032e4f279051763c066c8cb67ee8b2a4b
Nikita Averyanov <rac1onal@yandex.ru>
2023-08-12 12:57:11 +0300

    refactor: вынести RegExUrl в отдельный файл

--- middlewares/JoiValidator.js
+++ middlewares/JoiValidator.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { celebrate, Joi } = require('celebrate');
 
-const RegExUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\\+\\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\\+\\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\\+~%\\/.\w-_]*)?\??(?:[-\\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+const RegExUrl = require('../utils/RegExUrl');
 
 const createUserJoiValidation = celebrate({
   body: Joi.object().keys({
