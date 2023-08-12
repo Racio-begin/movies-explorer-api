d3902fcf96bca12519a1c5dec50d76251c0d8a86
Nikita Averyanov <rac1onal@yandex.ru>
2023-08-12 13:29:43 +0300

    feat: подключить логирование к проекту

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const errorHandler = require('./middlewares/errorHandler');
 
 const routes = require('./routes/index');
 
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
@@ -20,8 +22,14 @@ app.use(express.json());
 // подключаемся к базе данных
 mongoose.connect(DB_URL);
 
+// подключаем логгер запросов
+app.use(requestLogger);
+
 // подключаем роуты
 app.use(routes);
+
+// подключаем логгер ошибок
+app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
