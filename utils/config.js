e125d9b83f9f9cb54e13ad1b93446245258bc082
Nikita Averyanov <rac1onal@yandex.ru>
2023-08-13 17:30:24 +0300

    fix: поправить условие отработки конфига config.js при продакшене

--- utils/config.js
+++ utils/config.js
@@ -8,8 +8,8 @@ const {
 } = process.env;
 
 const config = {
-  PORT: PORT || 3000,
-  DB_URL: DB_URL || 'mongodb://127.0.0.1:27017/bitfilmsdb',
+  PORT: NODE_ENV === 'production' ? PORT : 3000,
+  DB_URL: NODE_ENV === 'production' ? DB_URL : 'mongodb://127.0.0.1:27017/bitfilmsdb',
   JWT_SECRET: NODE_ENV === 'production' ? JWT_SECRET : 'four-seven-two-thousand-twenty-three-secret',
 };
 
